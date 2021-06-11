df47d4e279d6102f7d34f922ec128e1fa03932bd
Brad <brt.jst@gmail.com>
2021-06-11 13:52:41 -0700

    remove comments from script, change readme

--- README.md
+++ README.md
@@ -1,36 +1,11 @@
 # Password-Generator
-This project was less about generating random numbers and more about compiling data that can be accesed and applied. The Instructor and TAs provided much of the code and how to apply it. Trying other methods and logic led to various ways of collecting the data. The references below contain information evident on the project but some serve as a map of the ways I attempted to compile the neccesary data. 
+This is one of the first of many homework assignments from Coding Bootcamp. While this application is not complicated it serves as an example of basic javascript logic and is useful in the real world. The user can follow the prompts to generate a random password from 8 - 128 characters, including upper and lower case letters, numbers and "special" characters such as "#" and "%." I have changed the css to make it more aesthetically pleasing and I enjoy this particular application.
 
-Collaberated with Dan Lucas, Breon Sparrow, Scott Ray, Emily Dobson
+Github repo: https://github.com/bradfan/Password-Generator
 
-Referrences:
+Deployed Link: https://bradfan.github.io/Password-Generator/
 
-MDN:
-https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
-
-https://developer.mozilla.org/en-US/docs/Web/API/Window/prompt
-
-https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/concat
-
-https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/concat
-
-https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/parseInt
-
-W3
-https://www.w3schools.com/js/js_loop_for.asp
-
-2ality:
-https://2ality.com/2017/04/conditional-literal-entries.html
-
-FreeCodeCamp.org
-
-youtube: 
-https://youtu.be/-xAJKmjKCUE
-https://youtu.be/NPbDqbwtr-4
-
-Instructor/Ofc Hours
-
-![alt text](screenshots/filename.png )
+![password-generator](./images/README.png)
 
 
 
--- script.js
+++ script.js
@@ -28,7 +28,7 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
        password = password.slice(0,isPasswordLength)
       passwordText.value = password;  
-       //  (already in homework - broken syntax broke code)
+      
 }
 
 // writePassword()
@@ -51,16 +51,16 @@ function writePassword() {
         // ref W3 schools for substitution of an array in "for Loop"
         // this abbreviated for loop determines how many times the if satement is run. covered in ofc hours.
        for (let i= 0; i< isPasswordLength; i++) {
-      if (isSpecialCharacters ===true){
+      if (isSpecialCharacters === true){
         password += getRandomItem(specialCharacters)
       }
-      if (isNumericCharacters ===true){
+      if (isNumericCharacters === true){
         password += getRandomItem(numericCharacters)
       }
-      if (isLowerCasedCharacters ===true){
+      if (isLowerCasedCharacters === true){
         password += getRandomItem(lowerCasedCharacters)
       }
-      if (isUpperCasedCharacters ===true){
+      if (isUpperCasedCharacters === true){
         password += getRandomItem(upperCasedCharacters)
       } 
      }
@@ -80,26 +80,5 @@ function writePassword() {
     }
 
 
-
-   
-
-
-
-
-// GIVEN I need a new, secure password
-// WHEN I click the button to generate a password
-// THEN I am presented with a series of prompts for password criteria
-// WHEN prompted for password criteria
-// THEN I select which criteria to include in the password
-// WHEN prompted for the length of the password
-// THEN I choose a length of at least 8 characters and no more than 128 characters
-// WHEN prompted for character types to include in the password
-// THEN I choose lowercase, uppercase, numeric, and/or special characters
-// WHEN I answer each prompt
-// THEN my input should be validated and at least one character type should be selected
-// WHEN all prompts are answered
-// THEN a password is generated that matches the selected criteria
-// WHEN the password is generated
-// THEN the password is either displayed in an alert or written to the page.
 
 
